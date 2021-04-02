2eb735a14eacfc4c53c6a28aa1eca6ea807a522b
Agouai <sam.suoan@gmail.com>
2021-04-02 18:22:37 +0800

    把money模块化

---
+++ src/views/money/Money.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout class-prefix="layout">
+    <number-pad/>
+    <types/>
+    <notes/>
+    <tags/>
+  </Layout>
+</template>
+
+
+<script lang="ts">
+import NumberPad from '@/views/money/numberPad'
+import Tags from '@/views/money/tags'
+import Types from '@/views/money/types'
+import Notes from '@/views/money/notes'
+
+export default {
+  name: 'Money',
+  components: {Notes, Types, Tags, NumberPad},
+}
+</script>
+
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+
+<style lang="scss" scoped>
+</style>
+
