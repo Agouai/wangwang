c1eeff92d1c3aa435bead2914113800d84b79be8
sam <sam.suoan@gmail.com>
2021-05-12 14:38:52 +0800

    把record保存至list

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -61,6 +61,8 @@ export default class NumberPad extends Vue {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   ok() {
     this.$emit('update:value', this.output);
+    this.$emit('submit',this.output)
+    this.output='0'
   }
 }
 </script>
